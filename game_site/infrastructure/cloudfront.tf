086a284076bce8d82f190d892e3119a5b029af65
moltm3 <moltm3@uic.edu>
2025-12-19 19:55:58 -0600

    update url with cert (non-tf)

--- game_site/infrastructure/cloudfront.tf
+++ game_site/infrastructure/cloudfront.tf
@@ -51,9 +51,9 @@ resource "aws_cloudfront_origin_access_control" "default" {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name = aws_s3_bucket.site_files.bucket_regional_domain_name
+    domain_name              = aws_s3_bucket.site_files.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
-    origin_id = local.s3_origin_id
+    origin_id                = local.s3_origin_id
   }
 
   enabled             = true
@@ -62,16 +62,16 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   web_acl_id = "arn:aws:wafv2:us-east-1:442426877818:global/webacl/CreatedByCloudFront-629d68d0/6854365c-4f7e-409d-a656-7b2e122dafe9"
 
-  # aliases = ["escaperoom.${local.my_domain}"]
+  aliases = ["escaperoom.uaqapps.com"]
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
 
     target_origin_id = local.s3_origin_id
-    compress = true
+    compress         = true
 
-    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6" # default `CachingOptimized`
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6" # default `CachingOptimized`
     viewer_protocol_policy = "redirect-to-https"
   }
   restrictions {
@@ -81,9 +81,9 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
-    # acm_certificate_arn = data.aws_acm_certificate.uaqapps.arn
-    # ssl_support_method  = "sni-only"
+    acm_certificate_arn      = "arn:aws:acm:us-east-1:442426877818:certificate/e20ee627-9144-4913-aa46-f34a05e9a3ee"
+    minimum_protocol_version = "TLSv1.3_2025"
+    ssl_support_method       = "sni-only"
   }
 }
 
