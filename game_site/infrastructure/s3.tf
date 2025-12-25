a5326a4a12db05a5b4307f31554596f011c99885
moltm3 <moltm3@uic.edu>
2025-12-24 23:23:27 -0600

    import, dir fix, add invalidations

--- game_site/infrastructure/s3.tf
+++ game_site/infrastructure/s3.tf
@@ -18,3 +18,21 @@ resource "aws_s3_object" "add_files" {
   # etag makes the file update when it changes; see https://stackoverflow.com/questions/56107258/terraform-upload-file-to-s3-on-every-apply
   etag = filemd5(each.value.source_path)
 }
+
+action "aws_cloudfront_create_invalidation" "invalidation" {
+  config {
+    distribution_id = aws_cloudfront_distribution.example.id
+    paths           = [ for name, file in module.template_files.files : name ]
+  }
+}
+
+resource "terraform_data" "trigger_invalidation" {
+  input = "trigger-invalidation"
+
+  lifecycle {
+    action_trigger {
+      events  = [before_create, before_update]
+      actions = [action.aws_cloudfront_create_invalidation.invalidation]
+    }
+  }
+}
