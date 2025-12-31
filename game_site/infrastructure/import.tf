acb67169f5085e0688409a07c088a7a4638b19f5
moltm3 <161519567+moltm3@users.noreply.github.com>
2025-12-31 15:00:17 -0600

    ER-24/feat(auto): improvement on invalidations (#14)

--- game_site/infrastructure/import.tf
+++ game_site/infrastructure/import.tf
@@ -21,3 +21,25 @@ import {
   to = aws_cloudfront_distribution.s3_distribution
   id = "E14BUX1WO7R2RY"
 }
+
+import {
+  to = aws_s3_object.add_files["todo.html"]
+  identity = {
+    bucket = "er-site-files"
+    key    = "todo.html"
+  }
+}
+import {
+  to = aws_s3_object.add_files["style.css"]
+  identity = {
+    bucket = "er-site-files"
+    key    = "style.css"
+  }
+}
+import {
+  to = aws_s3_object.add_files["index.html"]
+  identity = {
+    bucket = "er-site-files"
+    key    = "index.html"
+  }
+}
--- game_site/infrastructure/s3.tf
+++ game_site/infrastructure/s3.tf
@@ -4,29 +4,56 @@ resource "aws_s3_bucket" "site_files" {
 
 module "template_files" {
   source = "hashicorp/dir/template"
-
   base_dir = "../build"
 }
 
+# list of current object names
+data "aws_s3_objects" "current_keys" {
+  bucket = "er-site-files"
+}
+
+# data of all current objects
+data "aws_s3_object" "current_files" {
+  for_each = toset(data.aws_s3_objects.current_keys.keys)
+
+  bucket       = aws_s3_bucket.site_files.id
+  key          = each.key
+}
+
+locals {
+  # md5 of remote objects
+  remote_tags = { for name, file in data.aws_s3_object.current_files : name => file.etag }
+}
+
+
 resource "aws_s3_object" "add_files" {
   for_each = module.template_files.files
+  # always wait to do this until after current file md5s are read
+  depends_on = [ data.aws_s3_object.current_files ]
 
   bucket       = aws_s3_bucket.site_files.id
   key          = each.key
   source       = each.value.source_path
   content_type = each.value.content_type
+
   # etag makes the file update when it changes; see https://stackoverflow.com/questions/56107258/terraform-upload-file-to-s3-on-every-apply
   etag = filemd5(each.value.source_path)
+}
+
+locals {
+  # files changed or added
+  changes = compact([ for name, file in resource.aws_s3_object.add_files : lookup(local.remote_tags, name, "") != file.etag ? "/${name}" : null])
 }
 
 action "aws_cloudfront_create_invalidation" "invalidation" {
   config {
     distribution_id = aws_cloudfront_distribution.s3_distribution.id
-    paths           = [ for name, file in module.template_files.files : name ]
+    paths           = local.changes
   }
 }
 
 resource "terraform_data" "trigger_invalidation" {
+  count = length(local.changes) > 0 ? 1 : 0
   input = "trigger-invalidation"
 
   lifecycle {
